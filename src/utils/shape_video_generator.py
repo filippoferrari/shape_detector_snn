bd4666a0098ba2d3a50955c979bc67c657adeaa5
filippoferrari <ferrari.filippo@outlook.com>
2019-02-11 00:38:13 +0000

    Different shapes generator

--- src/utils/shape_video_generator.py
+++ src/utils/shape_video_generator.py
@@ -4,27 +4,28 @@ import numpy as np
 
 
 def main(args):
-    width = 128
-    height = 128
+    width = 32
+    height = 32
     FPS = 24
     seconds = 10
-    radius = 32
+    radius = 5
     bar_width = 1
     paint_h = int(height/2)
+    colour = (255, 255, 255)
 
     fourcc = cv2.VideoWriter_fourcc(*'MP42')
     video = cv2.VideoWriter('./' + args.shape.lower() +'.avi', fourcc, float(FPS), (width, height))
 
     for _ in range(0,3):
         for paint_x in range(-radius, width+radius):
-            frame = np.ones((height, width, 3), dtype=np.uint8)*255
+            frame = np.zeros((height, width, 3), dtype=np.uint8)
 
             if args.shape == 'circle':
-                cv2.circle(frame, (paint_x, paint_h), radius, (0, 0, 0), 1)
+                cv2.circle(frame, (paint_x, paint_h), radius, colour, 1)
             elif args.shape == 'square':
-                cv2.rectangle(frame, (paint_x, paint_h-radius//2), (paint_x+radius, paint_h+radius//2), (0,0,0), 3) 
+                cv2.rectangle(frame, (paint_x, paint_h-radius//2), (paint_x+radius-1, paint_h+radius//2), colour, -1) 
             elif args.shape == 'vertical':
-                cv2.rectangle(frame, (paint_x, paint_h-radius//2), (paint_x+bar_width, paint_h+radius//2), (0,0,0), -1) 
+                cv2.rectangle(frame, (paint_x, paint_h-radius//2), (paint_x+bar_width, paint_h+radius//2), colour, -1) 
 
             video.write(frame)
 

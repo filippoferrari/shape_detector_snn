6dc45547e486743396a1b149587a23ade9f8d21b
filippoferrari <ferrari.filippo@outlook.com>
2019-03-09 15:48:26 +0000

    Small changes to shape video generator

--- src/utils/shape_video_generator.py
+++ src/utils/shape_video_generator.py
@@ -8,13 +8,13 @@ def main(args):
     height = 32
     FPS = 24
     seconds = 10
-    radius = 7
+    radius = args.edge
     bar_width = 1
     paint_h = int(height/2)
     colour = (255, 255, 255)
 
     fourcc = cv2.VideoWriter_fourcc(*'MP42')
-    video = cv2.VideoWriter('./' + args.shape.lower() +'.avi', fourcc, float(FPS), (width, height))
+    video = cv2.VideoWriter('./' + args.shape.lower() + '_' + str(radius) + 'x' + str(radius) +'.avi', fourcc, float(FPS), (width, height))
 
     for _ in range(0,3):
         for paint_x in range(-radius, width+radius):
@@ -22,8 +22,10 @@ def main(args):
 
             if args.shape == 'circle':
                 cv2.circle(frame, (paint_x, paint_h), radius, colour, 1)
-            elif args.shape == 'square':
+            elif args.shape == 'square_lr':
                 cv2.rectangle(frame, (paint_x, paint_h-radius//2), (paint_x+radius-1, paint_h+radius//2), colour, 1) 
+            elif args.shape == 'square_tb':
+                cv2.rectangle(frame, (paint_h-radius//2, paint_x), (paint_h+radius//2 , paint_x+radius-1), colour, 1) 
             elif args.shape == 'vertical':
                 cv2.rectangle(frame, (paint_x, paint_h-radius//2), (paint_x+bar_width, paint_h+radius//2), colour, -1) 
 
@@ -36,6 +38,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-s', '--shape', default='circle', required=True, type=str, help='Shape in [\'circle\', \'square\']')
+    parser.add_argument('-e', '--edge', default=5, required=False, type=int, help='Length of the edge')
     args = parser.parse_args()
 
     return args

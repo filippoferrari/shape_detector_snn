9c7e070f60cf62d90e56e40f220800442f60e36d
filippoferrari <ferrari.filippo@outlook.com>
2019-03-10 22:12:10 +0000

    Remove useless print statements

--- src/network_utils/shapes.py
+++ src/network_utils/shapes.py
@@ -28,17 +28,14 @@ def vert_connections(r1, x, y, stride, r2):
 
     for j, w in enumerate(GAUSSIAN_WEIGHTS):
         gaussians = list(range(-(len(GAUSSIAN_WEIGHTS)//2), len(GAUSSIAN_WEIGHTS)//2 +1))
-        #if x == 0 and y == 0:  print(' V -------------')
         for i in range(-stride, stride + 1):
             # Left side
             pre_x = x-stride+gaussians[j]
             pre_y = y+i
-            #if x == 0 and y == 0: print(pre_x, pre_y)
             out.append((neuron_id(pre_x, pre_y, r1), neuron_id(x, y, r2), w, 1))
             # Right side
             pre_x = x+stride+gaussians[j]
             pre_y = y+i
-            #if x == 0 and y == 0: print(pre_x, pre_y)
             out.append((neuron_id(pre_x, pre_y, r1), neuron_id(x, y, r2), w, 1))
 
     out = [i for i in out if i[0] != []]
@@ -74,17 +71,14 @@ def hor_connections(r1, x, y, stride, r2):
 
     for j, w in enumerate(GAUSSIAN_WEIGHTS):
         gaussians = list(range(-(len(GAUSSIAN_WEIGHTS)//2), len(GAUSSIAN_WEIGHTS)//2 +1))
-        #if x == 0 and y == 0:  print(' H -------------')
         for i in range(-stride, stride + 1):
             # Top side 
             pre_x = x+i
             pre_y = y-stride+gaussians[j]
-            #if x == 0 and y == 0: print(pre_x, pre_y)
             out.append((neuron_id(pre_x, pre_y, r1), neuron_id(x, y, r2), w, 1))
             # Bottom side
             pre_x = x+i
             pre_y = y+stride+gaussians[j]
-            #if x == 0 and y == 0: print(pre_x, pre_y)
             out.append((neuron_id(pre_x, pre_y, r1), neuron_id(x, y, r2), w, 1))
 
     out = [i for i in out if i[0] != []]
@@ -115,9 +109,9 @@ def left_diag_connections(r1, x, y, stride, r2):
     out = []
 
     for i in range(0, 2 * stride + 1):
-        # Top side 
+        # Left side 
         out.append((neuron_id(x-2*stride+i, y+i, r1), neuron_id(x, y, r2), 1, 1))
-        # Bottom side
+        # Right side
         out.append((neuron_id(x+i, y-2*stride+i, r1), neuron_id(x, y, r2), 1, 1))
 
     out = [i for i in out if i[0] != []]
